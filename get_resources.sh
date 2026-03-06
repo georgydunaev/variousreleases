fca2540fe449f9ce41c832b2a1975380483807b2
Georgy Dunaev <georgedunaev@gmail.com>
2026-03-06 11:57:32 +0100

    fix

---
+++ get_resources.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#cp output.html ../../georgydunaev.github.io/output.html
+
+cp ../set_theory_prover/program/htmloutput.html htmloutput.html
+cp ../set_theory_prover/program/pdfoutput.pdf pdfoutput.pdf
+# cp ../CV/CV.pdf CV.pdf
+cp ../CV/CV_DE.pdf CV_DE.pdf
+cp ../CV/CV_EN.pdf CV_EN.pdf
+
+
